db750fabcdad277925a53fe6c70c1c69f96f11de
Apfeitoza <annapaulaasf@gmail.com>
2026-06-03 16:54:42 -0300

    feat: implementa score usando Sqlite proxy design pattern

--- code/Game.py
+++ code/Game.py
@@ -4,6 +4,8 @@ from code.Menu import Menu
 
 import pygame
 
+from code.Score import Score
+
 
 class Game:
     def __init__(self):
@@ -16,6 +18,8 @@ class Game:
             # aqui ele abre o menu
             menu = Menu(self.window)
             menu_return = menu.run()
+            #aqui ele recebe o score
+            score = Score(self.window)
 
             if menu_return in [MENU_OPTION[0], MENU_OPTION[1], MENU_OPTION[2]]:
                 player_score = [0,0] #[Player1, Player2]
@@ -24,7 +28,11 @@ class Game:
                 if level_return:
                     level = Level(self.window, 'Level2', menu_return, player_score)
                     level_return = level.run(player_score)
+                    if level_return:
+                        score.save(menu_return, player_score)
 
+            elif menu_return == MENU_OPTION[3]:
+                score.show()
             elif menu_return == MENU_OPTION[4]:
                 pygame.quit()  # Close window
                 quit()  # end pygame
